fa4400ccea3bef621beeaf8fc29fba59151de060
oluwagbenga adedire <oluwagbengaadedire@gmail.com>
2024-11-28 23:54:48 +0100

    fix

--- src/uicomponents/Logoswiper.jsx
+++ src/uicomponents/Logoswiper.jsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
-import { FreeMode, Pagination, Autoplay } from "swiper/modules";
+import Marquee from "react-fast-marquee";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+
 
 export default function LogoSwipper() {
   const [data, setData] = useState([]);
@@ -18,7 +15,6 @@ export default function LogoSwipper() {
       const response = await axios.get(
         "https://jobify-web-api.onrender.com/api/v1/sponsors/sponsors"
       );
-
       setData(response.data.sponsors);
     } catch (error) {
       setError(error.message);
@@ -41,33 +37,19 @@ export default function LogoSwipper() {
 
   return (
     <div>
-      <Swiper
-        slidesPerView={2}
-        spaceBetween={10}
-        freeMode={true}
-        autoplay={{
-          delay: 0,
-          disableOnInteraction: false,
-        }}
-        loop={true}
-        speed={8000}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination, Autoplay]}
-        className="mySwiper">
+      <Marquee gradient={false} speed={50}>
         {data.map((sponsor) => (
-          <SwiperSlide
+          <div
             key={sponsor._id}
-            className="flex items-center w-full h-full p-10">
+            className="flex items-center justify-center w-32 h-32 p-5 mx-4 bg-white shadow-lg rounded-lg">
             <img
               src={`https://jobify-web-api.onrender.com/${sponsor.logo}`}
               alt={sponsor.name}
-              className="object-scale-down w-32 h-32"
+              className="object-contain w-full h-full"
             />
-          </SwiperSlide>
+          </div>
         ))}
-      </Swiper>
+      </Marquee>
     </div>
   );
 }
