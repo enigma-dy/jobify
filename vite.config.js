469980767c81b4cf36713b3a31c74e0572375fdd
Oluwagbenge Adedire <adediredayo@gmail>
2024-11-28 10:58:07 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["swiper/react"],
+  },
   build: {
     rollupOptions: {
-      external: ["axios"],
+      external: ["axios", "swiper"],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
   },
 });
